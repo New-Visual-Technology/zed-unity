a4c0b2a13dd3cc14c25fb6e7f1736d885d9e270f
Max <tauss.maximilian@fronius.com>
2024-10-29 17:34:40 +0100

    Small null check

--- ZEDCamera/Assets/SDK/Helpers/Scripts/Utilities/UpgradePluginToSRP.cs
+++ ZEDCamera/Assets/SDK/Helpers/Scripts/Utilities/UpgradePluginToSRP.cs
@@ -18,7 +18,8 @@ public static class UpgradePluginToSRP
 
                 foreach (var cam in cameras)
                 {
-                    cam.transform.Find("Frame").GetComponent<MeshRenderer>().sharedMaterial = Mat_Zed_Forward_Lighting;
+                    if(cam.transform.Find("Frame") != null)
+                        cam.transform.Find("Frame").GetComponent<MeshRenderer>().sharedMaterial = Mat_Zed_Forward_Lighting;
                 }
 
                 if (cameras.Length > 0)
